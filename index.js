4ce56a23d412a51abf6261b52ac90cb079d545d1
Diego Cruz Garduño <diegoc.g56@gmail.com>
2025-02-02 13:29:54 -0600

    Hasta la lección 8

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+var name = "Diego Cruz"
+
+var friends = ["Juan", "Aaron", "Gael"];
+
+console.log(friends.length);
+console.log(friends[1]);
+
+var age =22;
+age = 1 + (age * 2);
+console.log(age);
